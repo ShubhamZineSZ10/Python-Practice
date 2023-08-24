fdc3cf64701fe1dfd81606f6353862d2e3463f43
Shubham Zine <110914885+ShubhamZineSZ10@users.noreply.github.com>
2023-08-24 13:23:54 +0530

    classes_objects

--- prog1.py
+++ prog1.py
@@ -1,3 +1,21 @@
-str1=input("-> ")
-print(str1)
-print(type(str1))
+# CREATING OBJECT 
+
+class Dog:
+
+    def __init__(self,name,age):
+        print("In constructor..")
+        self.name=name
+        self.age=age
+        
+        
+    def walk(self):
+        print("In walk function..")
+        print("name", self.name)
+        print("age", self.age)
+Lebra = Dog("Tommy",5)
+Pam = Dog("Harry",12)
+
+Lebra.walk()
+Pam.walk()
+print(Lebra.name)
+print(Pam.name)
